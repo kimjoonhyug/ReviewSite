b3ee786116bcd2dc660a2803cdd02b43d65fecc0
알렉스 <alexaquino@daum.net>
2018-07-17 14:56:35 +0900

    first commit

---
+++ src/main/java/com/teamcharm/review/ReviewApplication.java
@@ -0,0 +1,14 @@
+package com.teamcharm.review;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+                new SpringApplicationBuilder(ReviewApplication.class)
+				.properties("spring.config.name:application,config")
+				.build().run(args);
+	}
+}
