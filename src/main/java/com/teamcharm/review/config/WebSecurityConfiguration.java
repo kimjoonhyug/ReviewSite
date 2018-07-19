598940aa519a63d1915f17dc5b3092815f1e2a96
알렉스 <alexaquino@daum.net>
2018-07-19 16:05:34 +0900

    added /place to mapping

--- src/main/java/com/teamcharm/review/config/WebSecurityConfiguration.java
+++ src/main/java/com/teamcharm/review/config/WebSecurityConfiguration.java
@@ -33,7 +33,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 
                 .authorizeRequests()
-                .antMatchers("/home", "/register","/login","/store**").permitAll()
+                .antMatchers("/home", "/register","/login","/store**","/place").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .and()
