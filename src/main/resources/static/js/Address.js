1de04b77dab0976b280fc565184c66b0ac4279e7
알렉스 <alexaquino@daum.net>
2018-08-24 13:48:06 +0900

    lat lng js

---
+++ src/main/resources/static/js/Address.js
@@ -0,0 +1,27 @@
+function setLatLng(address) {
+    findAddressAndDo(address, callback);
+}
+
+function findAddressAndDo(address, callback) {
+    var geocoder = new daum.maps.services.Geocoder();
+
+    var options = {
+        page: 1,
+        size: 1
+    };
+
+    geocoder.addressSearch(address, callback, options);
+
+}
+
+function callback(result, status) {
+    if (status === daum.maps.services.Status.OK) {
+        //TODO set data into lat,lng input
+        console.log(result);
+        $('#lat').val(result.documents[0].road_address.y);
+        $('#lng').val(result.documents[0].road_address.x);
+    }
+}
+
+
+
